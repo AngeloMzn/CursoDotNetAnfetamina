95568ebe7a6ba598be58352135aac084389afaaa
Ângelo Vinicius Hernandez Mazarin <angelotrab22@gmail.com>
2023-01-16 15:40:26 -0300

    aula 1 do cursoadicionada

---
+++ aula1/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("**********************");
+//isso não faz nada
+Console.WriteLine("Hello, World!");
+
+/*
+    isso tambem nao faz nada 
+    fmkokfdflkdnfksd
+    dskmskmvklds´
+    sdfsdmvkakmdvdl
+    gflvsdlçm
+*/
+System.Console.WriteLine("***********************");
+
+int a;
+
